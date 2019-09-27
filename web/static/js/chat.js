3e454851c1591d28718de8bef4e46b844dd3b40d
Mrgoku348 <carlos.merma@utec.edu.pe>
2019-09-27 11:41:09 -0500

    jbksnb

--- web/static/js/chat.js
+++ web/static/js/chat.js
@@ -7,11 +7,11 @@ function get_all_users(){
         var i =0;
     $.each(data,function(){
         user_to=data[i]['id'];
-        e='<div class="alert" role="alert">';
-        e=e+'<div>' + data[i]['username']+'</div>';
-        e = e+'</div>'
+        e ='<div class="alert" role="alert">';
+        e =e+'<div>' + data[i]['username']+'</div>';
+        e = e+'</div>';
         i = i+ 1;
-        $("<div/>",{html:e}).appendto("#users")
+        $("<div/>",{html:e}).appendto("#users");
         });
     });
 }
