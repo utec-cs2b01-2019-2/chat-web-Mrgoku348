629661a117fc2ef1772db90bd2d6dcea18256859
Mrgoku348 <carlos.merma@utec.edu.pe>
2019-10-08 22:20:03 -0500

    c

---
+++ web/static/js/message.js
@@ -0,0 +1,41 @@
+$(function(){
+    var url = "http://127.0.0.1:8080/users";
+
+
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: url ,
+            insertUrl: url ,
+            updateUrl: url ,
+            deleteUrl: url ,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+        editing: {
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
+        remoteOperations: {
+            sorting: true,
+            paging: true
+        },
+        paging: {
+            pageSize: 12
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [8, 12, 20]
+        },
+        columns: [{
+            dataField: "id",
+            allowEditing: false
+        }, {
+            dataField: "username"
+        }, {
+            dataField: "message"
+        }, ],
+    }).dxDataGrid("instance");
+});
