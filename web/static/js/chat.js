19b7890e412e02c9015301a27c238f4fb73c33d4
Mrgoku348 <carlos.merma@utec.edu.pe>
2019-10-18 10:10:04 -0500

    Update chat.js

--- web/static/js/chat.js
+++ web/static/js/chat.js
@@ -1,25 +1,33 @@
 function sendMessage(){
     alert("Mensaje")
 }
-function sendMessage1() {
-    alert("corre")
+function get_current(){
+    console.log("Voy a traer el usuario logueado");
+    $.getJSON("/current", function(data){
+        console.log("Current user is "+data['username'])
+        get_all_users(data['id']);
+    });
 }
 
-function get_all_users(){
-    console.log("voy a traer todos los usuarios");
+function getMessages(user_from_id, user_to_id){
+    alert("Voy a traer los mensajes entre "+ user_from_id +" y "+ user_to_id);
+}
 
-    $.getJSON("/users",function(data){
-     var i =0;
-    $.each(data,function(){
+function get_all_users(user_from_id){
+    console.log("Voy a traer todos los usuarios");
+
+    $.getJSON("/users", function(data){
+    var i =0;
+    $.each(data, function(){
         user_to = data[i]['id'];
-        e ='<div class="alert" role="alert">';
+        e = '<div class="alert" role="alert" onclick="getMessages('+user_from_id+','+data[i]['id'] +')" >';
         e = e+'<div>'+data[i]['username']+'</div>';
-        e = e+"</div>";
-        i = i+ 1;
+        e = e+'</div>';
+        i = i+1;
         $("<div/>",{html:e}).appendTo("#users");
         });
     });
-};
+}
 function get_messages() {
     console.log("enviare mensajes");
     $.getJSON("users/",function (data) {
